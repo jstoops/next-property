bd37f104b5d5f80c27a1288efcac9df79fd19b19
John Stoops <john.stoops@gmail.com>
2025-01-15 11:30:47 -1000

    Add pagination & lightbox gallery

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,24 +1,39 @@
 import PropertyCard from '@/components/PropertyCard';
+import Pagination from '@/components/Pagination';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 
-const PropertiesPage  = async () => {
+const PropertiesPage = async ({ searchParams: { page = 1, pageSize = 9 } }) => {
   await connectDB();
-  const properties = await Property.find({}).lean();
-  
+
+  const skip = (page - 1) * pageSize;
+  const total = await Property.countDocuments({});
+  const properties = await Property.find({}).skip(skip).limit(pageSize);
+
+  const showPagination = total > pageSize;
+
   return (
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
-        { properties.length === 0 ? (<p>No properties found</p>) : (
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
           </div>
         )}
+        {showPagination && (
+          <Pagination
+            page={parseInt(page)}
+            pageSize={parseInt(pageSize)}
+            totalItems={total}
+          />
+        )}
       </div>
     </section>
   );
-}
- 
+};
+
 export default PropertiesPage;
